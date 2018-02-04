9b38819c551b87f2e6c76ece1bf6b4b8cbd28e0d
e155742 <e155742@ie.u-ryukyu.ac.jp>
2018-02-04 16:44:11 +0900

    add TImer class
    タイマーの内部操作をクラス化。
    MyFlameのtimeTextFieldを配列に。

--- src/main/java/net/rack/MyFrame.java
+++ src/main/java/net/rack/MyFrame.java
@@ -40,15 +40,17 @@ public class MyFrame extends JFrame implements Runnable, KeyListener, MouseListe
   public static final String FONT_NAME     = "Sans";
   public static final int    FONT_STYLE    = Font.BOLD;
   public static final int    FONT_SIZE     = 108;
-  public static final int    LOG_FONT_SIZE = 16;        // 右側の記録テーブルのフォントサイズ
+  public static final int    LOG_FONT_SIZE = 16;       // 右側の記録テーブルのフォントサイズ
   public static final int    BUTTON_WIDTH  = 150;
   public static final int    BUTTON_HEIGHT = 50;
-  public static final String ZERO_TIME     = "0:00.000";
 
-  private Container  contentPane           = getContentPane();
-  private JTextField lastLastTimeTextField = new JTextField(ZERO_TIME); // タイム表示上段
-  private JTextField lastTimeTextField     = new JTextField(ZERO_TIME); // タイム表示中段
-  private JTextField timeTextField         = new JTextField(ZERO_TIME); // タイム表示下段
+  Timer timer = new Timer();
+
+  private Container contentPane = getContentPane();
+
+  private JTextField[] timeTextField = { new JTextField(Timer.ZERO_TIME),
+                                         new JTextField(Timer.ZERO_TIME),
+                                         new JTextField(Timer.ZERO_TIME) };
 
   private DefaultTableModel tableModel = new DefaultTableModel(new String[] { "", "" }, 0) {
     @Override
@@ -60,10 +62,8 @@ public class MyFrame extends JFrame implements Runnable, KeyListener, MouseListe
   private JTable timeLogTable = new JTable(tableModel);                    // タイム表示横
   private Font   font         = new Font(FONT_NAME, FONT_STYLE, FONT_SIZE);
 
-  private long    startTime      = 0;
-  private int     logCount       = 0;
-  private boolean isRunning      = false;
-  private int     updateInterval = 77;
+  private int logCount       = 0;
+  private int updateInterval = 77;
 
   public MyFrame(String title, int widht, int eight) {
     setTitle(title);
@@ -80,9 +80,9 @@ public class MyFrame extends JFrame implements Runnable, KeyListener, MouseListe
 
   public void changeFontSize(int fontSize) {
     font = new Font(FONT_NAME, FONT_STYLE, fontSize);
-    lastLastTimeTextField.setFont(font);
-    lastTimeTextField.setFont(font);
-    timeTextField.setFont(font);
+    for (JTextField timeTextFields : timeTextField) {
+      timeTextFields.setFont(font);
+    }
   }
 
   private void setMargin(JMenu menu) {
@@ -143,11 +143,11 @@ public class MyFrame extends JFrame implements Runnable, KeyListener, MouseListe
       // @Override
       public void actionPerformed(ActionEvent e) {
         resetEvent();
-        startTime = 0;
         sb = new StringBuilder();
         df = new DecimalFormat();
-        lastLastTimeTextField.setText(ZERO_TIME);
-        lastTimeTextField.setText(ZERO_TIME);
+        for (int i = 1; i < timeTextField.length; i++) {
+          timeTextField[i].setText(Timer.ZERO_TIME);
+        }
         tableModel.setRowCount(0);
         logCount = 0;
       }
@@ -252,19 +252,15 @@ public class MyFrame extends JFrame implements Runnable, KeyListener, MouseListe
     JPanel panel = new JPanel();
     panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 
-    List<JTextField> textFieldList = new ArrayList<JTextField>();
-    textFieldList.add(lastLastTimeTextField);
-    textFieldList.add(lastTimeTextField);
-    textFieldList.add(timeTextField);
-    for (JTextField textFields : textFieldList) {
-      textFields.setAlignmentY(JComponent.CENTER_ALIGNMENT);
-      textFields.setHorizontalAlignment(JTextField.CENTER);
-      textFields.setPreferredSize(new Dimension(0, 0)); // フォントはみ出しても中央に配置
-      textFields.setEditable(false); // テキストフィールドの編集不可
-      textFields.setOpaque(false);
-      textFields.setFont(font);
-      panel.add(textFields);
-      textFields.addMouseListener(this);
+    for (int i = timeTextField.length - 1; i >= 0; i--) {
+      timeTextField[i].setAlignmentY(JComponent.CENTER_ALIGNMENT);
+      timeTextField[i].setHorizontalAlignment(JTextField.CENTER);
+      timeTextField[i].setPreferredSize(new Dimension(0, 0)); // フォントはみ出しても中央に配置
+      timeTextField[i].setEditable(false); // テキストフィールドの編集不可
+      timeTextField[i].setOpaque(false);
+      timeTextField[i].setFont(font);
+      panel.add(timeTextField[i]);
+      timeTextField[i].addMouseListener(this);
     }
     contentPane.add(panel, BorderLayout.CENTER);
   }
@@ -315,30 +311,26 @@ public class MyFrame extends JFrame implements Runnable, KeyListener, MouseListe
    *          - timeTextFieldにセットしたい文字列
    */
   synchronized public void setTimeTextField(String str) {
-    timeTextField.setText(str);
+    timeTextField[0].setText(str);
   }
 
   public void startEvent() {
-    if (isRunning) {
-      return;
-    }
-    startTime = System.nanoTime();
-    isRunning = true;
+    timer.starter();
     Thread thread = new Thread(MyFrame.this);
     thread.start();
   }
 
   public void goalEvent() {
-    if (!isRunning) {
+    if (!timer.getIsRunning()) {
       return;
     }
-    long goalTime = System.nanoTime();
-    isRunning = false;
-    long runTime = (goalTime - startTime);
-    String timeStr = timeFormatting(runTime);
+    timer.stopper();
+    String timeStr = timeFormatting(timer.getRunTime());
     setTimeTextField(timeStr);
-    lastLastTimeTextField.setText(lastTimeTextField.getText());
-    lastTimeTextField.setText(timeStr);
+    for (int i = timeTextField.length - 1; i > 0; i--) {
+      timeTextField[i].setText(timeTextField[i - 1].getText());
+    }
+
     tableModel.addRow(new Object[] { ++logCount, timeStr });
     int n = timeLogTable.convertRowIndexToView(tableModel.getRowCount() - 1);
     Rectangle r = timeLogTable.getCellRect(n, 0, true);
@@ -346,8 +338,8 @@ public class MyFrame extends JFrame implements Runnable, KeyListener, MouseListe
   }
 
   public void resetEvent() {
-    isRunning = false;
-    setTimeTextField(ZERO_TIME);
+    timer.reset();
+    setTimeTextField(Timer.ZERO_TIME);
   }
 
   // @Override
@@ -395,8 +387,8 @@ public class MyFrame extends JFrame implements Runnable, KeyListener, MouseListe
    */
   // @Override
   public void run() {
-    while (isRunning) {
-      setTimeTextField(timeFormatting(System.nanoTime() - startTime));
+    while (timer.getIsRunning()) {
+      setTimeTextField(timeFormatting(System.nanoTime() - timer.getStartTime()));
       try {
         Thread.sleep(updateInterval);
       } catch (InterruptedException e) {
---
+++ src/main/java/net/rack/Timer.java
@@ -0,0 +1,45 @@
+package net.rack;
+
+public class Timer {
+  public static final String ZERO_TIME = "0:00.000";
+
+  private long    startTime = 0;
+  private long    runTime   = 0;
+  private boolean isRunning = false;
+
+  public void starter() {
+    if (isRunning) {
+      return;
+    }
+    startTime = System.nanoTime();
+    isRunning = true;
+  }
+
+  public long getStartTime() {
+    return startTime;
+  }
+  
+  public boolean getIsRunning() {
+    return isRunning;
+  }
+
+  public void stopper() {
+    if (!isRunning) {
+      return;
+    }
+    long stopTime = System.nanoTime();
+    isRunning = false;
+    runTime = (stopTime - startTime);
+  }
+
+  public long getRunTime() {
+    return runTime;
+  }
+  
+  public void reset() {
+    startTime = 0;
+    runTime = 0;
+    isRunning = false;
+  }
+
+}
